0a2f6903a3349d716da5d9c5686002979ae12526
hasnabenouafel <161073788+hasnabenouafel@users.noreply.github.com>
2025-01-25 18:32:17 +0000

    Update Offer.jsx

--- Frontend/admin/Offer.jsx
+++ Frontend/admin/Offer.jsx
@@ -1,20 +1,19 @@
+import Header from "../../components/common/Header"; // Importing the Header component to display the page title
+import DailySalesTrend from "../../components/offers/DailySalesTrend"; // Importing the DailySalesTrend component to display the sales trend chart
 
-import Header from "../../components/common/Header";
-import DailySalesTrend from "../../components/offers/DailySalesTrend";
-const SalesPage
-= () => {
+const SalesPage = () => {
   return (
-    <div className='flex-1 overflow-auto relative z-10'>
-    <Header title='Products' />
-    
-    {/* USER CHARTS */}
-    <div className='grid grid-cols-0 lg:grid-cols-1 gap-6 mt-8'>
-					<DailySalesTrend />
-					
-				</div>
-		</div>
-
-  )
+    <div className="flex-1 overflow-auto relative z-10">
+      {/* Header component displaying the title of the page */}
+      <Header title="Products" />
+      
+      {/* USER CHARTS Section */}
+      <div className="grid grid-cols-0 lg:grid-cols-1 gap-6 mt-8">
+        {/* The DailySalesTrend component that shows the daily sales data chart */}
+        <DailySalesTrend />
+      </div>
+    </div>
+  );
 }
 
-export default SalesPage
+export default SalesPage;
